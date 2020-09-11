d224df62daaba15d9115bed3424297a0743cb4c3
matthewxvoorhees <matthew.crnich@gmail.com>
2020-09-11 10:18:01 -0600

    Few formatting issues cleared up

--- lib/api.rb
+++ lib/api.rb
@@ -6,26 +6,25 @@ require 'colorize'
 
 class API
 
-def self.scrape_characters
-resp = RestClient.get("https://ghibliapi.herokuapp.com/people/")
-ghibli_hash = JSON.parse(resp.body, symbolize_names:true)
+    def self.scrape_characters
+        resp = RestClient.get("https://ghibliapi.herokuapp.com/people/")
+        ghibli_hash = JSON.parse(resp.body, symbolize_names:true)
 
-ghibli_characters = ghibli_hash.collect do |person|
-    Character.new(person)
+    ghibli_characters = ghibli_hash.collect do |person|
+        Character.new(person)
+        end
     end
-end
 
-def self.scrape_details(character)
-    resp = RestClient.get(character.url)
-    char_hash = JSON.parse(resp.body, symbolize_names:true)
-    puts "Here is a little more about #{character.name}:".blue.on_white.bold
-    character.gender = char_hash[:gender]
-    character.age = char_hash[:age]
-    character.hair_color = char_hash[:hair_color]
-
-    character.films = char_hash[:films]
-    self.character_films(character)
-end
+    def self.scrape_details(character)
+        resp = RestClient.get(character.url)
+        char_hash = JSON.parse(resp.body, symbolize_names:true)
+        puts "Here is a little more about #{character.name}:".blue.on_white.bold
+        character.gender = char_hash[:gender]
+        character.age = char_hash[:age]
+        character.hair_color = char_hash[:hair_color]
+        character.films = char_hash[:films]
+        self.character_films(character)
+    end
 
     def self.character_films(character)
         resp = RestClient.get(character.films[0])
